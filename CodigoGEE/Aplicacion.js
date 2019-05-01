9ac93722cdcf9c7f3cb98ec394ff2f1d905080a7
Juan Diego <juandigoyra@gmail.com>
2019-05-01 18:19:34 +0200

    Añadidos algunos retoques estéticos y todos los rangos de todas las bandas
    - Añadida la etiqueta para mostrar la superficie invernada
    - Añadidos títulos a las secciones
    - Retocados algunos placeholders
    - Añadidos valores de rangos del resto de bandas que no se estaban teniendo en cuenta

--- CodigoGEE/Aplicacion.js
+++ CodigoGEE/Aplicacion.js
@@ -13,10 +13,11 @@ app.createPanels = function () {
       app.intro.title,
       app.intro.description
     ],
-    style: {width: '300px'}
+    style: { width: '300px' }
   });
 
   app.imageSelection = {
+    title: ui.Label('1) Find Near Images'),
     btn_findImages: ui.Button('Find Images', app.commands.findImages),
     selectWidget: ui.Select({
       items: [],
@@ -28,38 +29,45 @@ app.createPanels = function () {
     })
   };
   app.imageSelection.panel = ui.Panel([
+    app.imageSelection.title,
     app.imageSelection.btn_findImages,
     app.imageSelection.selectWidget,
     app.imageSelection.btn_addImage
   ]);
 
   app.imageAreaComputation = {
+    title: ui.Label('2) Image Manager'),
     selectWidget: ui.Select({
       items: app.model.selectedImages,
-      placeholder: 'Find images first',
+      placeholder: 'Find and Add any image first',
     }),
     btn_computeArea: ui.Button('Compute', app.commands.computeArea),
+    lbl_areaResult: ui.Label('Area:'),
     btn_drawImage: ui.Button('Draw', app.commands.drawImage)
   };
   app.imageAreaComputation.panel = ui.Panel([
+    app.imageAreaComputation.title,
     app.imageAreaComputation.selectWidget,
     app.imageAreaComputation.btn_computeArea,
+    app.imageAreaComputation.lbl_areaResult,
     app.imageAreaComputation.btn_drawImage
   ]);
 
   app.imageComparison = {
     //TODO
+    title: ui.Label('3) Compare Images'),
     slt_imageA: ui.Select({
       items: app.model.selectedImages,
-      placeholder: 'Pick an Image'
+      placeholder: 'Pick an added Image'
     }),
     slt_imageB: ui.Select({
       items: app.model.selectedImages,
-      placeholder: 'Pick a later Image'
+      placeholder: 'Pick an added more recent Image'
     }),
     btn_compare: ui.Button('Compare', app.commands.compareImages)
   };
   app.imageComparison.panel = ui.Panel([
+    app.imageComparison.title,
     app.imageComparison.slt_imageA,
     app.imageComparison.slt_imageB,
     app.imageComparison.btn_compare
@@ -124,7 +132,8 @@ app.createHelpers = function () {
 
     },
     comunicarAreaCalculada: function (areaExtension) {
-      print('Area covered: (in m2)', areaExtension);
+      var areaAsIntValue = parseInt(areaExtension);
+      app.imageAreaComputation.lbl_areaResult.setValue('GreenHose Area (in m2): '+areaAsIntValue);
     },
     buscarImagenes: function (startDate, endDate, onFoundImagesCallback) {
       var collectionID = app.constants.IMAGE_COLLECTION_ID;
@@ -147,8 +156,8 @@ app.createHelpers = function () {
     },
     representarDiferencias: function (invernaderosA, invernaderosB) {
       var normalizationFactor = 65535;
-      var visParamsIncrementoA = { min: 0, max: 30000/normalizationFactor, palette: ['FF0000'] };
-      var visParamsIncrementoB = { min: 0, max: 30000/normalizationFactor, palette: ['00FF00'] };
+      var visParamsIncrementoA = { min: 0, max: 30000 / normalizationFactor, palette: ['FF0000'] };
+      var visParamsIncrementoB = { min: 0, max: 30000 / normalizationFactor, palette: ['00FF00'] };
       var invernaderosAsinB;
       var invernaderosBsinA;
 
@@ -181,7 +190,7 @@ app.createHelpers = function () {
       var imageCollectionID = app.constants.IMAGE_COLLECTION_ID;
       var imageInCollectionID = app.imageSelection.selectWidget.getValue();
       var label = imageInCollectionID;
-      var uniqueImageID = imageCollectionID+'/'+imageInCollectionID;
+      var uniqueImageID = imageCollectionID + '/' + imageInCollectionID;
       var image = ee.Image(uniqueImageID);
       var selectedImages = app.model.selectedImages;
 
@@ -192,9 +201,9 @@ app.createHelpers = function () {
 
       //ACTUALIZAR LOS UI.WIDGETS QUE DEPENDEN DE LAS IMAGENES SELECCIONADAS
       selectedImages.push(object);
-      app.imageAreaComputation.selectWidget.items().reset( selectedImages );
-      app.imageComparison.slt_imageA.items().reset( selectedImages );
-      app.imageComparison.slt_imageB.items().reset( selectedImages );
+      app.imageAreaComputation.selectWidget.items().reset(selectedImages);
+      app.imageComparison.slt_imageA.items().reset(selectedImages);
+      app.imageComparison.slt_imageB.items().reset(selectedImages);
     },
     computeArea: function () {
       var image = app.imageAreaComputation.selectWidget.getValue();
@@ -218,8 +227,8 @@ app.createHelpers = function () {
       var invernaderosA;
       var invernaderosB;
 
-      invernaderosA = app.utils.detectarInvernaderos( imageA, region, rangesPerBand);
-      invernaderosB = app.utils.detectarInvernaderos( imageB, region, rangesPerBand);
+      invernaderosA = app.utils.detectarInvernaderos(imageA, region, rangesPerBand);
+      invernaderosB = app.utils.detectarInvernaderos(imageB, region, rangesPerBand);
 
       app.utils.representarDiferencias(invernaderosA, invernaderosB);
 
@@ -234,19 +243,41 @@ app.createConstants = function () {
   app.constants = {
     IMAGE_COLLECTION_ID: 'LANDSAT/LC08/C01/T1_RT_TOA',
     VISUALIZATION_PARAMS_NATURAL: { bands: ['B5', 'B4', 'B3'], min: 0, max: 30000 },
-    VISUALIZATION_PARAMS_NORMALIZED_NATURAL: { bands: ['B3'], min:0, max:30000/65535 },
+    VISUALIZATION_PARAMS_NORMALIZED_NATURAL: { bands: ['B3'], min: 0, max: 30000 / 65535 },
 
-    DEBUG_VISUALIZATION_PARAMS_BAND_3: {bands: ['B3'], min:0, max:30000}
+    DEBUG_VISUALIZATION_PARAMS_BAND_3: { bands: ['B3'], min: 0, max: 30000 }
   };
 
   app.model = {};
   app.model.selectedImages = [];
   app.model.getRangesPerBand = function () {
+    var normalize = true;
     var normalizationFactor = 65535;
-    return {
-      B3: { min: 25143/normalizationFactor, max: 27502/normalizationFactor },
-      B10: { min: 24930, max: 25389 }
+
+    var rangesPerBand = {
+      B1: { min: 23212, max: 24897 },
+      B2: { min: 24217, max: 26199 },
+      B3: { min: 25143, max: 27502 },
+      B4: { min: 27086, max: 30323 },
+      B5: { min: 30452, max: 33739 },
+      B6: { min: 24657, max: 28171 },
+      B7: { min: 20359, max: 23632 },
+      B8: { min: 25526, max: 28830 },
+      B9: { min: 5050, max: 5091 },
+      B10: { min: 24930, max: 25389 },
+      B11: { min: 23376, max: 23781 },
+      BQA: { min: 2800, max: 2800 }
     };
+
+    if(normalize){
+      var bandKey;
+      for(bandKey in rangesPerBand){
+        rangesPerBand[bandKey].min /= normalizationFactor;
+        rangesPerBand[bandKey].max /= normalizationFactor;
+      }
+    }
+
+    return rangesPerBand;
   };
   app.model.getExplorationZone = function () {
     return geometry;
